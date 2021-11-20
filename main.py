0eacaaab694fbd259787623df02b96526c46543c
Ruud Puts <>
2021-11-21 00:57:08 +0100

    Allow single task to run

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import importlib
 import inspect
 import os
 import re
+import sys
 
 def list_years():
     regex = re.compile(r"20[1|2]\d")
@@ -18,19 +19,33 @@ def list_days(year_module):
 
     return days
 
-if __name__ == "__main__":
-    years = list_years()
+def run_day(day_class):
+    day = day_class()
+    print("Day %s - %s" % (day.number(), day.title()))
+    day_input = day.read_input()
+    print("  Task 1: " + str(day.task1(day_input)))
+    print("  Task 2: " + str(day.task2(day_input)))
 
-    for year in years:
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        year = sys.argv[1]
+        day = sys.argv[2]
+
         print("Advent of Code " + year)
 
         year_module = importlib.import_module(year)
-
         for day_class in list_days(year_module):
-            print("")
+            if "Day%s" % day in str(day_class):
+                run_day(day_class)
+    else:
+        years = list_years()
 
-            day = day_class()
-            print("Day %s - %s" % (day.number(), day.title()))
-            day_input = day.read_input()
-            print("  Task 1: " + str(day.task1(day_input)))
-            print("  Task 2: " + str(day.task2(day_input)))
+        for year in years:
+            print("Advent of Code " + year)
+
+            year_module = importlib.import_module(year)
+
+            for day_class in list_days(year_module):
+                print("")
+
+                run_day(day_class)
