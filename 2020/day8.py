7b33723d37ec632d4c8ac75d78557b3e5df98808
Ruud Puts <>
2021-11-30 23:04:21 +0100

    Implement day 8 task 2

--- 2020/day8.py
+++ 2020/day8.py
@@ -16,19 +16,59 @@ class Day8(Day):
         return vm.accumulator
 
     def task2(self, input):
-        raise Exception("Task failed")
+        vm = VirtualConsole()
+        success = vm.run(input)
+        if success:
+            return vm.accumulator
+
+        patchable_instructions = {}
+        for offset, instruction in vm.instruction_history.items():
+            if instruction.operation == 'jmp':
+                patchable_instructions[offset] = instruction
+        
+        self.log("POSSIBLE PATCHES")
+        for offset, instruction in patchable_instructions.items():
+            self.log("  %d | %s %d" % (offset, instruction.operation, instruction.value))
+
+        patch_try = 0
+        while not success and patch_try < len(patchable_instructions):
+            instruction_to_patch = list(patchable_instructions.items())[patch_try]
+            patch_offset = instruction_to_patch[0]
+            
+            patched_instructions = input.copy()
+            patched_instructions[patch_offset] = Instruction('nop', instruction_to_patch[1].value)
+
+            success = vm.run(patched_instructions)
+            patch_try += 1
+
+        if success:
+            patch = list(patchable_instructions.items())[patch_try]
+            self.log("Found working patch: %d | %s %d" % (patch[0], patch[1].operation, patch[1].value))
+        else:
+            self.log("Exhausted all patch tries")
+
+        return vm.accumulator
 
 class VirtualConsole:
-    instruction_offset = 0
-    instruction_history = []
     accumulator = 0
+    instruction_offset = 0
+    instruction_history = {}
 
     def run(self, instructions):
-        while self.instruction_offset not in self.instruction_history:
+        self.reset()
+
+        while self.instruction_offset not in self.instruction_history.keys() and self.instruction_offset < len(instructions):
             instruction = instructions[self.instruction_offset]
-            self.instruction_history.append(self.instruction_offset)
+            self.instruction_history[self.instruction_offset] = instruction
 
             self.run_next_instruction(instruction)
+
+        return list(self.instruction_history.keys())[-1] == len(instructions) - 1
+
+    def reset(self):
+        self.accumulator = 0
+        self.instruction_offset = 0
+        self.instruction_history = {}
 
     def run_next_instruction(self, instruction):
         if instruction.operation == 'acc':
@@ -38,7 +78,6 @@ class VirtualConsole:
             self.instruction_offset += instruction.value
         else:
             self.instruction_offset += 1
-
 class Instruction:
     def __init__(self, operation, value):
         self.operation = operation
