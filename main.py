f4a9bf65cffb1e8aeeb06d6463902e4a2f90fc30
Ruud Puts <>
2021-12-07 17:26:34 +0100

    Read input separate for each task

--- main.py
+++ main.py
@@ -23,16 +23,15 @@ def list_days(year_module):
 def run_day(day_class):
     day = day_class()
     print("Day %s - %s" % (day.number(), day.title()))
-    day_input = day.read_input()
 
     task1_start = time.time()
-    task1_result = day.task1(day_input)
+    task1_result = day.task1(day.read_input())
     task1_end = time.time()
     task1_duration = task1_end - task1_start
     print("  Task 1: %s (took %fms)" % (str(task1_result), task1_duration))
 
     task2_start = time.time()
-    task2_result = day.task2(day_input)
+    task2_result = day.task2(day.read_input())
     task2_end = time.time()
     task2_duration = task2_end - task2_start
     print("  Task 2: %s (took %fms)" % (str(task2_result), task2_duration))
